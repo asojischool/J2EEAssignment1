7fd756e7092576db0eb94387adf6208120e68631
asojischool <asojischool@gmail.com>
2022-06-18 09:21:02 +0800

    User class commit 1

--- CA1Assignment/src/main/java/models/User.java
+++ CA1Assignment/src/main/java/models/User.java
@@ -1,5 +1,61 @@
 package models;
 
 public class User {
+	private int user_id;
+	private String username;
+	private String password;
+	private String role;
+	private String email;
+	
+	public User(int user_id, String username, String password, String role, String email) {
+		super();
+		this.user_id = user_id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.email = email;
+	}
 
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 }
