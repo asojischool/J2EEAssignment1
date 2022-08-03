d7e4ef370592217a2180884ff26ba14691f092d0
PYiJie <YIJIE.21@ichat.sp.edu.sg>
2022-08-03 21:45:09 +0800

    -

---
+++ CA1Assignment/src/main/java/servlets/custUpdate.java
@@ -0,0 +1,75 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.UserService;
+
+/**
+ * Servlet implementation class custUpdate
+ */
+@WebServlet("/custUpdate")
+public class custUpdate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public custUpdate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String idStr = request.getParameter("id");
+		int id = Integer.parseInt(idStr);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String role = request.getParameter("role");
+		String email = request.getParameter("email");
+		
+		if (username.equals("") || password.equals("") || role.equals("") || email.equals("") || username == null || password == null || role == null || email == null) {
+			request.setAttribute("err", "User Update Failed");
+			RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+			rd.forward(request, response);
+			return;
+		}
+		
+		UserService userService = new UserService();
+		int numRowsAffected = userService.custUpdate(id, username, password, role, email);
+		
+		if(numRowsAffected > 0) {
+			request.setAttribute("successMsg", "User Successsfully Updated");
+			RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+			rd.forward(request, response);
+			return;
+		}
+		else {
+			request.setAttribute("err", "User Update Failed");
+			RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+			rd.forward(request, response);
+			return;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ CA1Assignment/src/main/java/servlets/deleteUser.java
@@ -0,0 +1,63 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.UserService;
+
+/**
+ * Servlet implementation class deleteUser
+ */
+@WebServlet("/deleteUser")
+public class deleteUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public deleteUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String idStr = request.getParameter("id");
+		int id = Integer.parseInt(idStr);
+		UserService userService = new UserService();
+		int numRowsAffected = userService.deleteUser(id);
+		
+		if(numRowsAffected > 0) {
+			request.setAttribute("successMsg", "User Successsfully Deleted");
+			RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+			rd.forward(request, response);
+			return;
+		}
+		else {
+			request.setAttribute("err", "User Delete Failed");
+			RequestDispatcher rd = request.getRequestDispatcher("customer.jsp");
+			rd.forward(request, response);
+			return;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
